562dd34260944addf675359a2dce1766c7b051de
DevEben <ebenezertope4@gmail.com>
2025-03-10 06:13:40 +0100

    Add payment and notification features with webhook support

---
+++ models/transactionModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    email: {
+        type: String,
+    },
+    amount: { 
+        type: Number, 
+        required: true 
+    },
+    currency: { 
+        type: String, 
+        required: true 
+    },
+    method: {
+      type: String,
+      enum: ["flutterwave", "paypal", "stripe", "wise"],
+      required: true,
+    },
+    paymentType: {
+        type: String,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "successful", "failed"],
+      default: "pending",
+    },
+    reference: { 
+        type: String, 
+        unique: true 
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model("Transaction", TransactionSchema);
+
+module.exports = Transaction;
---
+++ utils/emailHandler/email.js
@@ -0,0 +1,53 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+async function sendMail(options) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: process.env.MAIL_SERVICE,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    // host: process.env.MAIL_SERVICE,
+    // port: 587,
+    // secure: false,
+    // auth: {
+    // user: process.env.MAIL_USER,
+    // pass: process.env.MAIL_PASS,
+    // },
+    // tls: {
+    //   rejectUnauthorized: false, // Helps with self-signed certificates
+    // },
+    });
+
+    const mailOption = {
+      from: process.env.MAIL_USER,
+      to: options.email,
+      subject: options.subject,
+      text: options.text,
+      html: options.html,
+      attachments: options.attachments, // Attachments array
+    };
+
+    await transporter.sendMail(mailOption);
+    return {
+      success: true,
+      message: "Email sent successfully",
+    };
+  } catch (Error) {
+    // Type guard to check if the error is an instance of Error
+    if (err instanceof Error) {
+      console.error("Error sending mail:", err.message);
+
+      return {
+        success: false,
+        message: "Error sending mail: " + err.message,
+      };
+    }
+  }
+}
+
+module.exports = { sendMail };
