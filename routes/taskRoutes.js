abe11ca08cda2cf4037a009509ea51103240703a
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-03-21 16:40:13 +0100

    Add changes to routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,23 +6,40 @@ const {
   deleteTaskHandler,
   getAllTasksHandler,
   getTaskCreatorTasksHandler,
+  searchAllTasksHandler,
+  postTaskHandler,
 } = require("../controllers/taskController");
 const upload = require("../middleware/multer");
 const isTaskCreator = require("../middleware/isTaskCreator");
 const { validation } = require("../middleware/auth");
 const isTaskEarner = require("../middleware/isTaskEarner");
-const { createTaskApplication, updateReviewStatus, updateEarnerStatus, fetchAllApplicationsEarner, fetchAllApplicationsCreator } = require("../controllers/taskApplicationController");
+const {
+  createTaskApplication,
+  updateReviewStatus,
+  updateEarnerStatus,
+  fetchAllApplicationsEarner,
+  fetchAllApplicationsCreator
+} = require("../controllers/taskApplicationController");
 const isApplicationOwner = require("../middleware/isApplicationOwner");
 const isTaskOwner = require("../middleware/isTaskOwner");
 const taskOwner = require("../middleware/taskOwner");
 
+
+// Tasks Routes
 route.post("/", validation, isTaskCreator, upload.single('addInfo'), createTaskHandler);
 route.patch("/:taskId", validation, isTaskCreator, taskOwner, upload.single('addInfo'), updateTaskHandler);
 route.delete("/:taskId", validation, isTaskCreator, taskOwner, deleteTaskHandler);
-route.get("/all", validation, getAllTasksHandler);
+route.get("/all", validation, isTaskEarner, getAllTasksHandler);
 route.get("/", validation, isTaskCreator, getTaskCreatorTasksHandler);
 
-// Task Applications
+// Search for available tasks (earner)
+route.post("/search", validation, isTaskEarner, searchAllTasksHandler);
+
+// Post an already created task (creator)
+route.patch("/:taskId/post", validation, isTaskCreator, taskOwner, postTaskHandler);
+
+
+// Task Applications Routes
 
 // Apply for a task (earner)
 route.post("/:taskId/applications", validation, isTaskEarner, createTaskApplication);
