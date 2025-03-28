e0d8e867a5f17e156cdfa0e96781ed226a8640ae
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-03-28 03:09:26 +0100

    Add popular tasks analysis endpoint

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { validation } = require('../middleware/auth');
 const isTaskCreator = require('../middleware/isTaskCreator');
-const { getAnalyticsOverview, getCompletedTasksOverTime } = require('../controllers/analyticsControllers');
+const { getAnalyticsOverview, getCompletedTasksOverTime, getPopularTasksAnalysis } = require('../controllers/analyticsControllers');
 
 const route = express.Router();
 
 route.get("/overview", validation, isTaskCreator, getAnalyticsOverview);
 route.get("/tasks-over-time", validation, isTaskCreator, getCompletedTasksOverTime);
-route.get("/popular-tasks", validation, isTaskCreator,);
+route.get("/popular-tasks", validation, isTaskCreator, getPopularTasksAnalysis);
 route.get("/worker-engagement", validation, isTaskCreator,);
 route.get("/task-duration", validation, isTaskCreator,);
 
