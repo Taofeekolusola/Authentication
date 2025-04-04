ef08cdd91002a43af10d5d875d2421a2a80d2380
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2025-04-04 19:40:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ connectDB();
 const allowedOrigins = [
     'http://localhost:3000',
     'https://altbucks-ipat.vercel.app',
-    'https://alt-bucks.vercel.app/',
+    'https://alt-bucks.vercel.app',
     'https://authentication-1-bqvg.onrender.com'
 ];
 
